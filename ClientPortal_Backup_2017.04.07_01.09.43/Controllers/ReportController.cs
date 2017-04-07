9343a0effb1219e08647eed457c3681eaf2442c4
Erwin Li <eli@arrow.ca>
2017-04-07 14:27:33 -0700

    Initial Commit

---
+++ ClientPortal_Backup_2017.04.07_01.09.43/Controllers/ReportController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClientPortal.Controllers
+{
+    public class ReportController : Controller
+    {
+        // GET: Report
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult InboundReceiving()
+        {
+            return View();
+        }
+
+        public ActionResult Inventory()
+        {
+            return View();
+        }
+
+        public ActionResult InventoryAnalysis()
+        {
+            return View();
+        }
+
+        public ActionResult OutboundShipping()
+        {
+            return View();
+        }
+       
+    }
+}
